e11a733a10a5c4b1be45261fb5cf2e20e8cb8030
Evan Higgins <evan.higgins06@gmail.com>
2017-05-13 23:09:37 -0400

    Created uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+unlink ${HOME}/.tmux.conf
+unlink ${HOME}/.vim
+unlink ${HOME}/.zshrc
+
+function deletepath {
+echo "About to delete $1"
+read -p "Would you like to continue? [Y/n]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]?$ ]]; then
+    echo "Deleting $1"
+    rm -rf $1
+fi
+}
+
+deletepath ${HOME}/.vim-install
+deletepath ${home}/.oh-my-zshrc
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+deletepath ${SCRIPTDIR}
+
